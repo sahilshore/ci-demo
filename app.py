b7cf677a89a3c4e0a756a907231150a0d94b21a1
Sahil Shore <sahilkhan08052002@gmail.com>
2026-09-25 18:52:20 +0530

    add square fuction

--- app.py
+++ app.py
@@ -16,13 +16,19 @@ def divide(a, b):
     else:
         return "Error: Division by zero is not allowed."
 
-    
+
+def square(a):
+    return a * a
+
+
 result1 = add(5, 3)
 result2 = subtract(10, 4)
 result3 = multiply(6, 7)
 result4 = divide(10, 2)
+result5 = square(5)
 
 print("Addition Result:", result1)
 print("Subtraction Result:", result2)
 print("Multiplication Result:", result3)
 print("Division Result:", result4)
+print("Square Result:", result5)
