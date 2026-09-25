3a6923925cd1d1ef536576fa6b21159060cb4dcc
Sahil Shore <sahilkhan08052002@gmail.com>
2026-09-25 17:51:02 +0530

    fix linting issue

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
+def subtract(a, b):
+    return a - b
 
 
+def multiply(a, b):
+    return a * b
 
-
-
-
-
-
-
-
-
-
-
-
-def subtract(a,b):return a-b
-def multiply(a,b):
-    return a*b
 result1 = add(5, 3)
 result2 = subtract(10, 4)
 result3 = multiply(6, 7)
