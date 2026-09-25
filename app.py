3445488bab11bd97d1ae3a3ad44c4aa81682810f
Sahil Shore <sahilkhan08052002@gmail.com>
2026-09-25 17:27:10 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+def add(a,b):
+    return a+b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def subtract(a,b):return a-b
+def multiply(a,b):
+    return a*b
+result1 = add(5, 3)
+result2 = subtract(10, 4)
+result3 = multiply(6, 7)
+
+print("Addition Result:", result1)
+print("Subtraction Result:", result2)
+print("Multiplication Result:", result3)
