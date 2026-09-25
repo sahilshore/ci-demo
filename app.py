a24fb728d14594ab9ba5cdba4f981db0a954fb72
Sahil Shore <sahilkhan08052002@gmail.com>
2026-09-25 18:08:19 +0530

    fix flake8 errors

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 def add(a, b):
     return a + b
+
+
 def subtract(a, b):
     return a - b
+
+
 def multiply(a, b):
     return a * b
+
+
 result1 = add(5, 3)
 result2 = subtract(10, 4)
 result3 = multiply(6, 7)
