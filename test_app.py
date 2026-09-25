054273185d427c3ea9b7d2d3a064d09b1e4a376e
Sahil Shore <sahilkhan08052002@gmail.com>
2026-09-25 19:29:29 +0530

    custom logic

---
+++ test_app.py
@@ -0,0 +1,30 @@
+# test_app.py
+
+import unittest
+from app import add, subtract, multiply, divide, square
+
+class TestMathFunctions(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(4, 5), 9)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_sub(self):
+        self.assertEqual(subtract(4, 5), -1)
+        self.assertEqual(subtract(-1, -1), 0)
+
+    def test_mul(self):
+        self.assertEqual(multiply(4, 5), 20)
+        self.assertEqual(multiply(-1, -1), 1)
+
+    def test_div(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(10, 0), "Error: Division by zero is not allowed.") 
+
+    def test_square(self):
+        self.assertEqual(square(4), 16)
+        self.assertEqual(square(-3), 9)
+        self.assertEqual(square(0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
